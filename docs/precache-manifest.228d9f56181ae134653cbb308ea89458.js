08446527e48747f4ffe8fe35071637f9468ff6aa
33750900053607 <47789111+33750900053607@users.noreply.github.com>
2019-07-24 18:08:57 +0900

    disc-logo

--- docs/precache-manifest.228d9f56181ae134653cbb308ea89458.js
+++ docs/precache-manifest.228d9f56181ae134653cbb308ea89458.js
@@ -1,7 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ce9ef79f2b33f29c4b96c44277f5fc3d",
+    "revision": "cbf79bbda2562eab009ca48dac7ba6bd",
     "url": "./index.html"
+  },
+  {
+    "revision": "a0f8511a78667f3766ed",
+    "url": "css/app.css"
   },
   {
     "revision": "589f22db3ab56eb2cfc548d104f6d6f7",
@@ -36,43 +40,43 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "fonts/MaterialIcons-Regular.woff2"
   },
   {
-    "revision": "6260a6bd0d3207a6ad94",
+    "revision": "a0f8511a78667f3766ed",
     "url": "js/app.js"
   },
   {
-    "revision": "45eef3f1b826d54e2bf0bf0225b21eb2",
+    "revision": "eb8f650b91e838b23c595a65e33d53e2",
     "url": "manifest.json"
   },
   {
     "revision": "b08f600e75284d6274ee0ee03f2ed1b3",
-    "url": "static\\icons\\128x128.png"
+    "url": "static/icons/128x128.png"
   },
   {
     "revision": "78bb1bad56b56f9176976fbb19f640d8",
-    "url": "static\\icons\\144x144.png"
+    "url": "static/icons/144x144.png"
   },
   {
     "revision": "09bac65cbc73f494482c480006f7c31a",
-    "url": "static\\icons\\152x152.png"
+    "url": "static/icons/152x152.png"
   },
   {
     "revision": "1c41296078623fe115c3b966852a9759",
-    "url": "static\\icons\\192x192.png"
+    "url": "static/icons/192x192.png"
   },
   {
     "revision": "ad809d0a19d880fc189e41e9726df58e",
-    "url": "static\\icons\\256x256.png"
+    "url": "static/icons/256x256.png"
   },
   {
     "revision": "9fa40ac188452505f216044f8c7a9b88",
-    "url": "static\\icons\\512x512.png"
+    "url": "static/icons/512x512.png"
   },
   {
     "revision": "14235e3018bb57cc700c799c5f9ef1be",
-    "url": "static\\icons\\apple-touch-icon.png"
+    "url": "static/icons/apple-touch-icon.png"
   },
   {
     "revision": "c88219dd9796cca0bfa90e472a805ab9",
-    "url": "static\\icons\\favicon.png"
+    "url": "static/icons/favicon.png"
   }
 ]);
